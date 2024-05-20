21e568839e952265714a642ac4cd204874ded03b
TuNombreDeUsuario <pabloacisera2021@gmail.com>
2024-05-20 13:16:09 -0300

    commit: confiugracion de cors

--- src/app.ts
+++ src/app.ts
@@ -1,34 +1,94 @@
-import express from 'express'
-import { API_VERSION, NODE_ENV, PORT } from "./config/config";
+import express from "express";
+import { API_VERSION, LOG_FORMAT, NODE_ENV, PORT } from "./config/config";
 import { Routes } from "./interfaces/route.interface";
-import { logger } from './utilities/logger';
+import { logger } from "./utilities/logger";
+import displayRoutes from "express-routemap";
+import morgan from "morgan";
+import cors from "cors";
+import hpp from "hpp";
+import helmet from "helmet";
+import cookieParser from 'cookie-parser'
+import  corsConfig  from "./config/cors.config";
+import  {stream}  from './utilities/logger'
 
 class App {
   public app: express.Application;
   public env: string;
   public port: number | string;
+  public server: any;
 
-  constructor(routes:Routes[]){
-    this.app=express();
-    this.env= NODE_ENV || 'development';
+  constructor(routes: Routes[]) {
+    this.app = express();
+    this.env = NODE_ENV || "development";
     this.port = Number(PORT) || 5000;
 
-    this.initialiteRoute(routes)
+    this.initializeRoute(routes);
+    this.connectToDatabase();
+    this.initializeSwagger();
+    this.initializeMiddlewares();
+    this.initializeErrorHandlers();
   }
 
-  public initialiteRoute(routes:Routes[]){
-    routes.forEach((route)=>{
-      this.app.use(`/api/${API_VERSION}`, route.router)
-    })
+  /**
+   * getServer
+   */
+  public getServer() {
+    return this.app;
   }
 
-  public listen(){
-    this.app.listen(this.port, ()=>{
-      logger.info('==============================================')
-      logger.info(`===============env:${this.env}================`)
-      logger.info(`=======App listinig on port ${PORT}=============`)
-      logger.info('==============================================')
-    })
+  /**
+   * closeServer
+don?:any   */
+  public closeServer(done?: any) {
+    this.server = this.app.listen(this.port, () => {
+      done();
+    });
+  }
+
+  public initializeRoute(routes: Routes[]) {
+    routes.forEach((route) => {
+      this.app.use(`/api/${API_VERSION}`, route.router);
+    });
+  }
+
+  /**
+   * connectToDatabase
+   */
+  private connectToDatabase() {}
+
+  /**
+   * initialiteMiddlewares
+   */
+  private initializeMiddlewares() {
+
+    this.app.use(morgan(LOG_FORMAT ?? '../logs', { stream }));
+    this.app.use(cors(corsConfig));
+    this.app.use(hpp());
+    this.app.use(helmet());
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: true }));
+    this.app.use(cookieParser());
+
+  }
+
+  /**
+   * initialiteSwagger
+   */
+  private initializeSwagger() {}
+
+  /**
+   * initialiteErrorHandlers
+   */
+  public initializeErrorHandlers() {}
+
+  public listen() {
+    this.app.listen(this.port, () => {
+      displayRoutes(this.app);
+      logger.info("==============================================");
+      logger.info(`===============env:${this.env}================`);
+      logger.info(`=======App listinig on port ${PORT}=============`);
+      logger.info("==============================================");
+    });
   }
 }
 
---
+++ src/config/cors.config.ts
@@ -0,0 +1,10 @@
+import { ORIGIN } from "./config";
+
+const corsConfig=()=>{
+  allowedHeaders: "*";
+  methods: "GET, POST, HEAD, PATCH, PUT, DELETE";
+  preflightContinue: false;
+  origin: ORIGIN;
+}
+
+export default corsConfig;
